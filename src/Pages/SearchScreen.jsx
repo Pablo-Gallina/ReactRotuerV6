157f49da5d84952b0d55ba308118ff06a394f5ea
Pablo Gallina <pablogallinag@gmail.com>
2021-12-03 11:32:43 -0600

    Variable agregada a la url al buscar

--- src/Pages/SearchScreen.jsx
+++ src/Pages/SearchScreen.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SearchScreen = () => {
+const SearchScreen = ({history}) => {
+    const [search, setSearch] = useState("")
+    const handleChange = (e)=>{
+        const value = e.target.value;
+        setSearch(value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        //Agregar un query param al path (para realizar la busqueda)
+        history.push(`?q=${search}`)
+    }
     return (
         <>
             <h1>Search</h1>   
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Buscar
+                    <input type="text" onChange={handleChange} value={search} placeholder="Nombre" />
+                </label>
+                <button className="btn btn-primary">Buscar</button>
+            </form>
         </>
     )
 }
