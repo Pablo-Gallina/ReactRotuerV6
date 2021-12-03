37cab5dc896c0a5bf13520536f7fa5a478bd052a
Pablo Gallina <pablogallinag@gmail.com>
2021-12-03 11:02:46 -0600

    Agregando datos a la vista de character

--- src/Pages/CharacterScreen.jsx
+++ src/Pages/CharacterScreen.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import { Characters } from '../models/Characters';
 
 export const CharacterScreen = () => {
     //Desescructurando el parametro
     const {id} = useParams();
 
+    const {type, name, description} = Characters.find(character => character.id === id);
+
+    const path = `/assets/${type}-${id}.png`;
+
     return (
         <div>
           <h1>{id}</h1>  
+          <img src={path} alt={id} />
         </div>
     )
 }
