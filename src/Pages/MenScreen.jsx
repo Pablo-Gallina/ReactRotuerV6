250cc7f96f1f0b5d42d69e49acac9b711d51b33e
Pablo Gallina <pablogallinag@gmail.com>
2021-12-03 10:51:56 -0600

    Cargar datos en el componente card

--- src/Pages/MenScreen.jsx
+++ src/Pages/MenScreen.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Card from '../components/Card/Card';
+import { Characters } from '../models/Characters'
 
 const MenScreen = () => {
+    const men = Characters.filter(character => character.type === "h");
     return (
         <>
             <h1>Men</h1>   
+            <div className="container">
+                <div>
+                    {men.map(_men => <Card key={_men.id} {..._men} />)}
+                </div>
+            </div>
         </>
     )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+const Card = ({id, name, description, type}) => {
+    const path = `/assets/${type}-${id}.png`;
+    return (
+        <div>
+            <h1>{name}</h1>
+            {/* <img src={path} alt={id} /> */}
+            <Link className="card-link" to={""}>
+            VER MAS...
+            </Link>
+        </div>
+    )
+}
+
+export default Card
