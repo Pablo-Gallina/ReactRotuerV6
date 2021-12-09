7e19f233d4efd462e8de96515e45a3ceaa946933
Pablo Gallina <pablogallinag@gmail.com>
2021-12-08 18:14:50 -0600

    Configurando el estado para compartir en toda la aplicacion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
+
+import { AuthContext } from './context/AuthContext'
+import { AuthReducer } from './reducers/AuthReducer'
+
 import LoginRouter from './routes/LoginRouter'
 
+// Estado inicial
+const init = () => {
+  return JSON.parse(localStorage.getItem('log')) || {log: false}
+}
+
 const App = () => {
+  // reducer para el auth
+  const [log, dispatch] = useReducer(AuthReducer, {}, init)
+
+  // Lanzar un efecto cada vez que log (el que contola el estado del auth) cambie
+  useEffect(() => {
+    localStorage.setItem('log', log)
+  }, [log])
+
   return (
-    <>
-      <LoginRouter/>
-    </>
+    // Compartir el estado en toda la aplicacion
+    <AuthContext.Provider value={{ log, dispatch }}>
+      <LoginRouter/> { /* Switch con las rutas (Router principal) */ }
+    </AuthContext.Provider>
   )
 }
 
