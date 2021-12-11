58449437bc1e2459856018a7c73bc7dd8fdc49e8
Pablo Gallina <pablogallinag@gmail.com>
2021-12-10 18:39:19 -0600

    React Lazy & Suspense

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
+
 import Navbar from '../components/Navbar/Navbar'
-import { CharacterScreen } from '../Pages/CharacterScreen'
-import MenScreen from '../Pages/MenScreen'
-import SearchScreen from '../Pages/SearchScreen'
-import WomenScreen from '../Pages/WomenScreen'
+import Spinner from '../components/Spinner/Spinner'
+// import { CharacterScreen } from '../Pages/CharacterScreen'
+// import MenScreen from '../Pages/MenScreen'
+// import SearchScreen from '../Pages/SearchScreen'
+// import WomenScreen from '../Pages/WomenScreen'
 
-
+const CharacterScreen = lazy(() => import("../Pages/CharacterScreen"));
+const MenScreen = lazy(() => import("../Pages/MenScreen"));
+const SearchScreen = lazy(() => import("../Pages/SearchScreen"));
+const WomenScreen = lazy(() => import("../Pages/WomenScreen"))
 
 const LoginRouter = () => {
     return (
         <>
-        <Navbar />
-        <Switch>
-            <Route exact path="/men" component={MenScreen} />
-            <Route exact path="/women" component={WomenScreen} />
-            <Route exact path="/search" component={SearchScreen} />
+            <Navbar />
+            <Suspense fallback={<Spinner />}>
+                <Switch>
+                    <Route exact path="/men" component={MenScreen} />
+                    <Route exact path="/women" component={WomenScreen} />
+                    <Route exact path="/search" component={SearchScreen} />
 
-            {/* Ruta con parametro (el parametro es id) */}
-            <Route exact path="/character/:id" component={CharacterScreen} />
+                    {/* Ruta con parametro (el parametro es id) */}
+                    <Route exact path="/character/:id" component={CharacterScreen} />
 
-            <Redirect to="/men" />
-        </Switch>
+                    <Redirect to="/men" />
+                </Switch>
+            </Suspense>
         </>
     )
 }
